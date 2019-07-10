494198cf7611338af68ee5144197be13cc2268b4
Your Name <you@example.com>
2019-07-10 20:10:45 +0300

    first commit

---
+++ 4/routing/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+ @import "http://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+ body {
+   background: rgb(223, 223, 223);
+   font-family: Arial, Helvetica, sans-serif;
+ }
+ html,body {
+   height: calc(100% - 50px);
+ }
+ #app{
+   background: #fff;
+   width: 50%;
+   padding: 30px;
+   border-radius: 10px;
+   margin: 50px auto 0 auto;
+   height: calc(60% - 50px);
+ }
+</style>
---
+++ 4/routing/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+    <h1>This is an about page</h1>
+  </div>
+</template>
